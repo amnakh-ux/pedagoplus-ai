68ddbc5de76969c53d17458405294025aa013760
amnabk <amnababbokh@gmail.com>
2026-09-08 19:24:31 +0100

    Use Prisma WASM client on Cloudflare

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/wasm.js";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const connectionString = process.env.DATABASE_URL;
